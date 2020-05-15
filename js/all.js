aeac301157bb113fd0c4660813e77156f0dee1d2
chiakilalala <chiaki205102@gmail.com>
2020-05-15 21:51:18 +0800

    order

---
+++ js/all.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    // 導覽列按鈕
+    $('#nav-toggle').on('click', function(e) {
+
+            $('#nav-content').toggleClass('hidden')
+                // $('.js-m-navbar__list').toggleClass('is-active')
+            e.preventDefault()
+        })
+        // 導覽列按鈕 end
+
+    // 固定導覽列
+    const setFixed = $('#header')
+    let menuTop = setFixed.offset().top
+
+    $(window).on('load scroll resize', function() {
+            if ($(window).scrollTop() > menuTop) {
+                setFixed.addClass('is-fixtop')
+            } else {
+                setFixed.removeClass('is-fixtop')
+            }
+        })
+        // 固定導覽列 end
+
+    // 至頂按鈕
+    const backtopBtn = $('.js-e-backtop-btn')
+
+    backtopBtn.hide()
+
+    backtopBtn.on('click', function() {
+        $('html,body').animate({ scrollTop: 0 }, 'slow')
+        return false
+    })
+    $(window).scroll(function() {
+            if ($(this).scrollTop() > 400) {
+                backtopBtn.fadeIn()
+            } else {
+                backtopBtn.fadeOut()
+            }
+        })
+        // 至頂按鈕 end
+})
