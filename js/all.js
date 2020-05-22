56aae44c1ebd776ad86a27071a559b453d04aaca
chiakilalala <chiaki205102@gmail.com>
2020-05-22 11:14:56 +0800

    update

--- js/all.js
+++ js/all.js
@@ -2,54 +2,90 @@ $(document).ready(function() {
     // 導覽列按鈕
     $('#nav-toggle').on('click', function(e) {
 
-            $('#nav-content').toggleClass('hidden')
-                // $('.js-m-navbar__list').toggleClass('is-active')
-            e.preventDefault()
-        })
-        // 導覽列按鈕 end
+        $('#nav-content').toggleClass('hidden');
+        // $('.js-m-navbar__list').toggleClass('is-active')
+        e.preventDefault();
+    });
+    // 導覽列按鈕 end
 
     // 固定導覽列
     const setFixed = $('#header');
     const setbutton = $('#navAction');
-    const setlogo = $('.toggleColour');
+
+    const changeLogo = $('#changeLogo');
     let menuTop = setFixed.height();
     // setFixed.offset().top
 
-    $(window).on('load scroll resize', function() {
-            if ($(window).scrollTop() > menuTop) {
-                setFixed.addClass('is-fixtop');
-                setbutton.addClass('bg-blue-500');
-                setlogo.addClass('text-blue-500');
+    let navScroll = {
 
-            } else {
+        init: function() {
+            $(window).on('scroll', function() {
+                navScroll.navDrop();
+            })
+        },
+
+        navDrop: function() {
+            let $scrollTop = $(window).scrollTop();
+
+            if ($scrollTop > menuTop) {
+                setFixed.addClass('is-fixtop');
+                setbutton.addClass('bg-blue-500 text-white');
+                // setbutton.removeClass('text-gray-800');
+
+                changeLogo.addClass('changeLogo');
+            } else if ($scrollTop == 0) {
                 setFixed.removeClass('is-fixtop');
-                setbutton.removeClass('bg-blue-500');
-                setlogo.removeClass('text-blue-500');
+                setbutton.removeClass('bg-blue-500 text-white');
+                // setbutton.addClass('text-gray-800');
+
+                changeLogo.removeClass('changeLogo');
             }
-        })
-        // 固定導覽列 end
+
+        }
+    };
+    $(document).ready(function() {
+        navScroll.init();
+    });
+
+    // $(window).on('load scroll resize', function () {
+    //     if ($(window).scrollTop() > menuTop) {
+    //         setFixed.addClass('is-fixtop');
+    //         setbutton.addClass('bg-blue-500 text-white');
+    //         // setbutton.removeClass('text-gray-800');
+    //         setlogo.addClass('text-blue-500');
+    //         changeLogo.addClass('changeLogo');
+
+    //     } else {
+    //         setFixed.removeClass('is-fixtop');
+    //         setbutton.removeClass('bg-blue-500 text-white');
+    //         // setbutton.addClass('text-gray-800');
+    //         setlogo.removeClass('text-blue-500');
+    //         changeLogo.removeClass('changeLogo');
+    //     }
+    // });
+    // 固定導覽列 end
 
     // 至頂按鈕
     const backtopBtn = $('.js-e-backtop-btn')
 
-    backtopBtn.hide()
+    backtopBtn.hide();
 
     backtopBtn.on('click', function() {
         $('html,body').animate({ scrollTop: 0 }, 'slow')
-        return false
+        return false;
     });
     $(window).scroll(function() {
             if ($(this).scrollTop() > 400) {
-                backtopBtn.fadeIn()
+                backtopBtn.fadeIn();
             } else {
-                backtopBtn.fadeOut()
+                backtopBtn.fadeOut();
             }
         })
         // 至頂按鈕 end
 });
 var helpMenuDiv = document.getElementById("menu-content");
 var helpMenu = document.getElementById("menu-toggle");
-
+// helpMenu.addEventListener('click', check);
 document.onclick = check;
 //Help Menu
 function check(e) {
